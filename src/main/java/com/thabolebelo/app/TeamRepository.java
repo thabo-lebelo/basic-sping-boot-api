6baaf2575f725f5910c3b8feb82f9c373b49fd41
thabolebelo <thabo.lebelo@absa.africa>
2019-07-16 11:06:04 +0200

    Add Team entity for testing purposes

---
+++ src/main/java/com/thabolebelo/app/TeamRepository.java
@@ -0,0 +1,11 @@
+package com.thabolebelo.app;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    Team findByFirstName(String firstName);
+
+}
---
+++ src/test/java/com/thabolebelo/app/TeamRepositoryIntegrationTest.java
@@ -0,0 +1,37 @@
+package com.thabolebelo.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+import static org.assertj.core.api.Assertions.*;
+
+@DataJpaTest
+@RunWith(SpringRunner.class)
+public class TeamRepositoryIntegrationTest {
+
+    @Autowired
+    private TestEntityManager entityManager;
+
+    @Autowired
+    private TeamRepository teamRepository;
+
+    @Test
+    public void whenFindByName_returnEmployee() {
+//        //given this details
+//        Team touch = new Team((long) 5,"Thabo", "Lebelo", "Developer");
+//
+//        entityManager.persist(touch);
+//        entityManager.flush();
+//
+//        //when this occurs
+//        Team found = teamRepository.findByFirstName(touch.getFirstName());
+//
+//        //then
+//        assertThat(found.getFirstName()).isEqualTo(touch.getFirstName());
+
+    }
+
+}
